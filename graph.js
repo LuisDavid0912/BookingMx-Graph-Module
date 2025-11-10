49d109adc936602d29f804b0e43fd690c43b0b06
Luis David Martínez Gutiérrez <luisdavidmtz3@gmail.com>
2025-11-10 17:58:16 -0600

    Graph Tests

--- graph.js
+++ graph.js
@@ -1,7 +1,43 @@
-function findNearbyCities(graph, startCity) {
+const cityGraph = {
+  'CDMX': [
+    { city: 'Queretaro', distance: 213 },
+    { city: 'Puebla', distance: 130 }
+  ],
+  'Queretaro': [
+    { city: 'CDMX', distance: 213 },
+    { city: 'San Luis Potosi', distance: 205 },
+    { city: 'Guadalajara', distance: 330 }
+  ],
+  'Puebla': [
+    { city: 'CDMX', distance: 130 },
+    { city: 'Veracruz', distance: 280 }
+  ],
+  'San Luis Potosi': [
+    { city: 'Queretaro', distance: 205 },
+    { city: 'Monterrey', distance: 512 }
+  ],
+  'Guadalajara': [
+    { city: 'Queretaro', distance: 330 },
+    { city: 'Tepic', distance: 208 }
+  ],
+  'Veracruz': [
+    { city: 'Puebla', distance: 280 }
+  ],
+  'Monterrey': [
+    { city: 'San Luis Potosi', distance: 512 }
+  ],
+  'Tepic': [
+    { city: 'Guadalajara', distance: 208 }
+  ]
+};
 
-  return [];
+function findNearbyCities(graph, startCity) {
+  
+  if (!graph[startCity]) {
+    return [];
+  }
+
+  return graph[startCity];
 }
 
-
-module.exports = { findNearbyCities };
+module.exports = { findNearbyCities, cityGraph };
--- graph.test.js
+++ graph.test.js
@@ -1,13 +1,36 @@
-const { findNearbyCities } = require('./graph.js');
+const { findNearbyCities, cityGraph } = require('./graph.js');
 
 
 describe('Graph Module - findNearbyCities', () => {
 
-  test('1. (TODO) should return nearby cities correctly', () => {
-    const graph = {}; 
+  test('1. should return nearby cities for an existing city', () => {
     const city = 'CDMX';
+    const expectedResult = [
+      { city: 'Queretaro', distance: 213 },
+      { city: 'Puebla', distance: 130 }
+    ];
 
-    const nearby = findNearbyCities(graph, city);
+    const nearby = findNearbyCities(cityGraph, city);
+
+    expect(nearby).toEqual(expectedResult); 
+  });
+
+
+
+  test('2. should return an empty array for a non-existent city', () => {
+    const city = 'Cancun'; 
+
+    const nearby = findNearbyCities(cityGraph, city);
+
+
+    expect(nearby).toEqual([]); 
+  });
+
+
+  test('3. should return an empty array for invalid input (null)', () => {
+    const city = null;
+
+    const nearby = findNearbyCities(cityGraph, city);
 
     expect(nearby).toEqual([]);
   });
